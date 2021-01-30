926acc07e2c00177923964aab9a11ab749bd3723
vomichaelminh <vomm@uci.edu>
2021-01-29 20:02:13 -0800

    .

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 // import userRoutes from "./routes/user.js";
+// import eventRoutes from "/routes/event.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,11 @@ app.use(cors());
 
 // routes
 // app.use("/users", userRoutes);
+// app.use("/events", eventRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 mongoose.connect(
   process.env.CONNECT_URL,
