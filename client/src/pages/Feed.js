af1a4fcdf7d6525840022d27700407f1855c5847
Jerry Tan <jerry88tan@gmail.com>
2021-01-30 10:03:28 -0800

    add temp navbar for feed

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -3,13 +3,12 @@ import Event from '../components/Event'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import NavBar from '../components/NavBar'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginTop: 100,
   },
   paper: {
     padding: theme.spacing(2),
@@ -126,6 +125,8 @@ export const Feed = () => {
 
         
         <div>
+
+            <NavBar/>
             <div className={classes.root}>
       <Grid container spacing={3}>
       {eventData.map((user) => (
