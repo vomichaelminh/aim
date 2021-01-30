a3a8b2c754a2e76d828e73083fbe529000d91ee9
vomichaelminh <vomm@uci.edu>
2021-01-29 19:42:49 -0800

    cleaned files

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// import userRoutes from "./routes/user.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// routes
+// app.use("/users", userRoutes);
+
+mongoose.connect(
+  process.env.CONNECT_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("Connected to db!");
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
