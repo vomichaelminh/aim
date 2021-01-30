0bc4016c653d94547e87411713b15db3bddd37c6
Jerry Tan <jerry88tan@gmail.com>
2021-01-30 00:28:04 -0800

    add post request to send token to endpoint

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,12 +3,19 @@ import Google from "../images/Google.png";
 import "../styles/Login.css";
 import Button from "@material-ui/core/Button";
 import { GoogleLogin } from 'react-google-login';
+import axios from 'axios';
 
 const clientId =
   '1065315695970-i9fhvuus3sgaq27t0v385pgn0376qeco.apps.googleusercontent.com';
 
   const responseGoogle = (response) => {
     console.log(response);
+    axios.post('http://localhost:5000/api/googleLogin', {
+      tokenId: response.tokenId,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
   }
 
 export const Login = () => {
